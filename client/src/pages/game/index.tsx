359f71489fd3fccec6c7024fff012ca17a6af79e
jja6312 <127708710+jja6312@users.noreply.github.com>
2025-01-18 21:45:04 +0900

    feat:게임 방생성시, 1P 정보를 프론트에 함께 넘겨주기 구현

--- client/src/pages/game/index.tsx
+++ client/src/pages/game/index.tsx
@@ -1,45 +1,49 @@
-
-import { useState } from "react";
-import GameLogin from "./webSocketTest/GameLogin";
-import GameScreen from "./webSocketTest/GameScreen";
-import GameInviteOrParticipation from "./webSocketTest/GameInviteOrParticipation";
+import { useState } from 'react';
+import GameLogin from './webSocketTest/GameLogin';
+import GameScreen from './webSocketTest/GameScreen';
+import GameInviteOrParticipation from './webSocketTest/GameInviteOrParticipation';
 
 const GamePage = () => {
   const [isLogin, setIsLogin] = useState(false); // 로그인 여부
   const [isGameStart, setIsGameStart] = useState(false); // 게임 시작
-  const [member,setMember] = useState({
-    id:"",
-    name:"",
-    email:""
+  const [member, setMember] = useState({
+    // 로그인을 통해 set되고, 방생성시 사용되는 member
+    id: '',
+    name: '',
+    email: '',
   });
-  
-  
-  return <>
-  
-  {
-  // 1. 로그인이 되지 않았다면 로그인 화면 출력.
-  !isLogin ?   
-  <GameLogin 
-  isLogin = {isLogin}
-  setIsLogin = {setIsLogin}
-  setMember={setMember}
-  />
-  
-  
-  // 2. 로그인을 했고, 게임 시작을 진행하지 않았다면 게임초대혹은입장장 화면 출력.
-  : !isGameStart ? 
-  <GameInviteOrParticipation 
-  isGameStart = {isGameStart}
-  setIsGameStart = {setIsGameStart}
-  member = {member}
-  
-  />
 
-  // 3. 게임 시작이 되면 게임 화면 출력.
-  : <GameScreen />}
-  
-  
-  </>;
+  //================ 인수인계 이후 내용 ================
+  const [roomStatus, setRoomStatus] = useState(''); //방 생성시 READY 로 바뀐다.
+  const [players, setPlayers] = useState({
+    // 방생성시 1P가 정해진다. 초대링크를통해 접근한다면 2P가 정해진다.
+    player1P: { memberId: 0, name: '' },
+    player2P: { memberId: 0, name: '' },
+  });
+  //===================================================
+
+  return (
+    <>
+      {
+        // 1. 로그인이 되지 않았다면 로그인 화면 출력.
+        !isLogin ? (
+          <GameLogin isLogin={isLogin} setIsLogin={setIsLogin} setMember={setMember} />
+        ) : // 2. 로그인을 했고, 게임 시작을 진행하지 않았다면 게임초대혹은입장장 화면 출력.
+        !isGameStart ? (
+          <GameInviteOrParticipation
+            isGameStart={isGameStart}
+            setIsGameStart={setIsGameStart}
+            member={member}
+            setRoomStatus={setRoomStatus}
+            setPlayers={setPlayers}
+          />
+        ) : (
+          // 3. 게임 시작이 되면 게임 화면 출력.
+          <GameScreen roomStatus={roomStatus} players={players} />
+        )
+      }
+    </>
+  );
 };
 
 export default GamePage;
--- client/src/pages/game/webSocketTest/api/createRoom.jsx
+++ client/src/pages/game/webSocketTest/api/createRoom.jsx
@@ -1,11 +1,11 @@
-import api from "../../../../api/instance";
+import api from '../../../../api/instance';
 
 export const createRoom = async (memberId) => {
-    const response = await api("http://localhost:8080/api/v1/game/room",{
-        method: "POST",
-        json: {memberId},
-    });
-    
-console.log("dddddddddresponse",response);
-    return response;
-}
+  const response = await api('http://localhost:8080/api/v1/game/room', {
+    method: 'POST',
+    json: { memberId },
+  });
+
+  console.log('방생성', response);
+  return response;
+};
